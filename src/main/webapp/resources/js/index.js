7c7f9c7baae4d54231310b2a784b5db58e37b909
Lester Wang <wanghuai28@outlook.com>
2015-07-24 19:18:13 +0800

    small test about how to use closure

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -4,3 +4,9 @@ function sayHi() {
 	var newHeader = goog.dom.createDom('h1', { 'style': 'backgroud-color: #EEE'}, 'Hello world!');
 	goog.dom.appendChild(document.body, newHeader);
 }
+
+(function(){
+	var buttonNode = document.getElementById('btn_first');
+	var button = new goog.ui.Button();
+	button.render(buttonNode);
+})();
